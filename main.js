48ce0098ac0b3c009ca14db77beed2dc281265c4
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2023-08-06 23:40:38 +0530

    Provided a year shows the calender of that year, default: 2021.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const { Month } = require('./src/calendar');
 
 const main = function () {
-  const jan = new Month(1, 2023);
-  console.log(jan.renderMonth());
-}
+  const year = process.argv[2] || 2021;
+  const months = [
+    'jan',
+    'feb',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',
+  ];
+
+  calender = months.map((_, index) => new Month(index + 1, year));
+  calender.forEach((month) => console.log(month.renderMonth(), '\n'));
+};
 
 main();
